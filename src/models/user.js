b83c6291e0298e32fcdbf66ab5ca4cb9f372936f
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-04-29 13:59:41 +0530

    login changed
    login changed

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    // email:{ type: String, required: true},
+    email:{ type: String, required: true},
     user_type: { type: Number, required: true, default: 0 }, // 0 - citizen, 1 - ofsc, 2 - okvi, 3 - osic
     user_role: { type: Number, required: true, default: 2 }, // 0 - admin, 1 - sub admin/ district admin, 2 - user
     // user_branch:{ type: String, required: true},
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,30 +4,30 @@ const User = require('../models/user');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-exports.registerUser = async ({ username, password }) => {
+exports.registerUser = async ({ username, email, password }) => {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword, user_type:0, user_role:2 });
+    const newUser = new User({ username, email, password: hashedPassword, user_type:0, user_role:2 });
     await newUser.save();
     return { message: 'User registered successfully' };
 };
 
-exports.register_ofsc_superadmin = async({ username, password }) => {
+exports.register_ofsc_superadmin = async({ username, email, password }) => {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword, user_type: 1, user_role:0});
+    const newUser = new User({ username, email, password: hashedPassword, user_type: 1, user_role:0});
     await newUser.save();
     return { message: 'User registered Successfully' };
 }
 
-exports.register_ofsc_subadmin = async({ username, password }) => {
+exports.register_ofsc_subadmin = async({ username, email, password }) => {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword, user_type: 1, user_role:1});
+    const newUser = new User({ username, email, password: hashedPassword, user_type: 1, user_role:1});
     await newUser.save();
     return { message: 'User registered Successfully' };
 }
 
 
-exports.loginUser = async ({ username, password }) => {
-    const user = await User.findOne({ username });
+exports.loginUser = async ({ email, password }) => {
+    const user = await User.findOne({ email });
     if (!user || !(await bcrypt.compare(password, user.password))) {
         throw new Error('Invalid credentials');
     }
