e1f4f0eda8c1051addd1009ba3419e2ab8b9193e
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-04-29 14:09:36 +0530

    another commit
    another commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: { type: String, required: true},
+    password: { type: String, required: true, unique: true },
     email:{ type: String, required: true},
     user_type: { type: Number, required: true, default: 0 }, // 0 - citizen, 1 - ofsc, 2 - okvi, 3 - osic
     user_role: { type: Number, required: true, default: 2 }, // 0 - admin, 1 - sub admin/ district admin, 2 - user
