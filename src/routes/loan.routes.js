b612dc9ad9881eb0b306603931a89b7936ddb2d4
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-05-26 19:22:04 +0530

    error rectified
    error rectified

--- src/routes/loan.routes.js
+++ src/routes/loan.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const loanController = require('../controllers/loanController');
+const loanController = require('../controllers/loan.controller');
 const { authenticateToken } = require('../middlewares/auth.middleware');
 
 // router.post('/', loanController.createLoan);
