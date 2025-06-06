49561065137422a8ce63c4fcc339d73f7916ee8b
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-06-06 17:20:52 +0530

    cascas
    cascascasc

--- src/validations/otsFormValidation.js
+++ src/validations/otsFormValidation.js
@@ -1,8 +1,8 @@
-// validations/otsFormValidation.js
 const Joi = require('joi');
+
 const otsFormSchema = Joi.object({
-    // applicationId: Joi.string().required(),
-    // userId: Joi.string().required(),
+    userId: Joi.string().required(),
+
     first_name: Joi.string().required(),
     last_name: Joi.string().required(),
     number: Joi.string().required(),
@@ -12,17 +12,32 @@ const otsFormSchema = Joi.object({
     city: Joi.string().required(),
     branch: Joi.string().required(),
     pin_code: Joi.string().required(),
+
     liability_ofsc: Joi.string().required(),
     industry: Joi.string().required(),
     financial_heights: Joi.string().required(),
     reason_ots: Joi.string().required(),
     payment_source: Joi.string().required(),
     terms_payment: Joi.string().required(),
+
+    level_of_operation: Joi.string().required(),
+    reason_non_payments: Joi.string().required(),
+    details_of_dues: Joi.string().required(),
+    status_of_npa: Joi.string().required(),
+    reason_for_settlement: Joi.string().required(),
+    sources_of_payment: Joi.string().required(),
+
+    loan_number: Joi.string().required(),
+
+    status: Joi.number().optional(), // default handled in Mongoose
+    status_msg: Joi.string().optional(), // default handled in Mongoose
+
     check_box1: Joi.boolean().required(),
     check_box2: Joi.boolean().required(),
     check_box3: Joi.boolean().required(),
     check_box4: Joi.boolean().required(),
-    any_others_particulars: Joi.string().allow('', null),
+
+    any_others_particulars: Joi.string().allow('', null)
 });
 
 module.exports = otsFormSchema;
