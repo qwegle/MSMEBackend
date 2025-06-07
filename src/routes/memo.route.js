ae1ee7dd511638cb4e2f22a13f2723eac0169047
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-06-07 20:22:49 +0530

    cdscs
    csdcsddcs

--- src/routes/memo.route.js
+++ src/routes/memo.route.js
@@ -5,7 +5,7 @@ const memorandumController = require('../controllers/memo.controller');
 const upload = require("../middlewares/fileUploadHandler");
 router.post('/uploadMemorandum', upload.single('pdf'), memorandumController.uploadPdf);
 router.post('/reuploadMemo', upload.single('pdf'), memorandumController.reuploadMemo);
-router.post('/updateMemoStatus', upload.single('pdf'), memorandumController.updateMemoStatus);
+router.post('/updateMemoStatus', memorandumController.updateMemoStatus);
 router.get('/getMemoById/:userId', memorandumController.getMemosByUserId);
 router.post('/filterMemos', memorandumController.getAllMemos);
 // router.get('/:id', memorandumController.getPdfById);
