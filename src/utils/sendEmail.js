b0bd3be93e63146eac8b1ea04867cfae9270a272
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-07-01 12:48:16 +0530

    vcsdvsdgv
    ergerg

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,15 +2,15 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: 'gmail',
     auth: {
-      user: process.env.EMAIL_FROM,
+      user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
   await transporter.sendMail({
-    from: process.env.EMAIL_FROM,
+    from: process.env.EMAIL_USER,
     to,
     subject,
     html,
