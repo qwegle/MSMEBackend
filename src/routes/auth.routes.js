2b3efcf87588f268fa03958d84ecde80599feff8
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-07-24 19:15:46 +0530

    verifyAppCheck, removed
     verifyAppCheck, removed

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,16 +11,16 @@ import { register,
     resendResetOTP, 
      } from '../controllers/auth.controller.js';
 import { authenticateToken} from '../middlewares/auth.middleware.js';
-import  verifyAppCheck  from '../middlewares/appcheckVerification.js'
+// import  verifyAppCheck  from '../middlewares/appcheckVerification.js'
 const router = Router();
 
-router.post('/register', verifyAppCheck, register);
-router.post('/login', verifyAppCheck, login);
+router.post('/register',  register);
+router.post('/login',  login);
 router.post('/register_ofsc_superadmin', register_OFSC_SuperAdmin);
 router.post('/register_ofsc_subadmin',register_OFSC_SubAdmin);
 router.post('/logout',authenticateToken, logout);
-router.post('/forgot-password', verifyAppCheck, forgotPassword);
-router.post('/reset-password', verifyAppCheck, resetPassword);
+router.post('/forgot-password',  forgotPassword);
+router.post('/reset-password',  resetPassword);
 router.post('/resend-otp', resendResetOTP);
 router.post('/update-password',authenticateToken, updateProfile);
 
