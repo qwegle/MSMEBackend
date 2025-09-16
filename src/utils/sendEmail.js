3dc3308abbf9908c49fd1abe6b9e1d6a3bbc13c7
SpandanQwegle <spandan.satapathy@qwegle.com>
2025-09-16 11:59:13 +0530

    this is
    Sendemail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,27 +2,20 @@ import { createTransport } from 'nodemailer';
 
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    host: 'smtp.sendgrid.net',
+    port: 587,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: 'apikey', // literally the string 'apikey'
+      pass: process.env.SENDGRID_API_KEY,
     },
   });
 
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      html,
-    });
-    console.log('Email sent successfully');
-  } catch (err) {
-    console.error('Error sending email:', err);
-    throw err;
-  }
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER, // this must be a verified sender in SendGrid
+    to,
+    subject,
+    html,
+  });
 };
 
 export default sendEmail;
